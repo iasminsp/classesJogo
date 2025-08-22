d3cbf3569eec17eac7e81b23d5ddbbc5870fe244
Iasmin De Souza Pereira <130411997+iasminsp@users.noreply.github.com>
2025-08-22 12:54:03 +0000

    desafio 3 DIO-Logica de programação

---
+++ classesJogo.js
@@ -0,0 +1,39 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+
+    switch (this.tipo) {
+      case "mago":
+        ataque = "usou magia";
+        break;
+      case "guerreiro":
+        ataque = "usou espada";
+        break;
+      case "monge":
+        ataque = "usou artes marciais";
+        break;
+      case "ninja":
+        ataque = "usou shuriken";
+        break;
+      default:
+        ataque = "usou um ataque indefinido";
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const heroiMago = new Heroi("Gandalf", 1500, "mago");
+heroiMago.atacar(); 
+const heroiGuerreiro = new Heroi("Conan", 35, "guerreiro");
+heroiGuerreiro.atacar(); 
+const heroiMonge = new Heroi("Bruce Lee", 28, "monge");
+heroiMonge.atacar(); 
+const heroiNinja = new Heroi("Hattori Hanzo", 25, "ninja");
+heroiNinja.atacar();
